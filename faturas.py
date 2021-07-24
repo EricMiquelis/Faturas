17eda180f46a9552b74307b749f0ac586d811050
EricMiquelis <67070626+EricMiquelis@users.noreply.github.com>
2021-07-24 14:57:48 -0300

    Add files via upload

---
+++ faturas.py
@@ -0,0 +1,7 @@
+cliente = str(input("Digite o nome do Cliente: "))
+dia = str(input("Digite o dia de vencimento: "))
+mes = str(input("Digite o mês de vencimento: "))
+valor_fatura = str(input("Degite o valor da fatura: "))
+
+print("Olá,",cliente)
+print("A sua fatura com vencimento em", dia, "de", mes, "no valor de R$", valor_fatura, "está fechada.")
